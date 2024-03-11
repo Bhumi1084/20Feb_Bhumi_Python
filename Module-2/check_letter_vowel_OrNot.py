73fa1e5af6f5048e96d65a4920d03e4294ab89c2
unknown <pambharbhumi108@gmail.com>
2024-03-11 15:50:59 +0530

    11-03-2024

---
+++ Module-2/check_letter_vowel_OrNot.py
@@ -0,0 +1,8 @@
+#Write a Python program to test whether a passed letter is a vowel or not.
+
+letter=input("Enter any letter : ")     #get value from user
+#check condition
+if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u' or letter=='A' or letter=='E' or letter=='I' or letter=='O' or letter=='U':
+    print("Letter is vowel.")       #condition are true then print this message.
+else:
+    print("Letter is not vowel.")   #condition are false then print this message.
