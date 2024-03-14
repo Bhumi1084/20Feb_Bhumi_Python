f88e4e9867d0868b732bd3c0d31aa13321de611e
unknown <pambharbhumi108@gmail.com>
2024-03-14 17:30:52 +0530

    14-03-2024

--- Module-2/reverses_string.py
+++ Module-2/reverses_string.py
@@ -1,10 +1,10 @@
 #Write a Python function to reverses a string if its length is a multiple of 4. 
 
-string=input("Enter a string : ")       #get value from user
+string=input("Enter a string : ")       #get value from user    #bhumi
 reverses_string = " "
 index = len(string) - 1
 
-while index >= 0:
-    reverses_string += string[index]
-    index -= 1
-print(reverses_string)
+while index >= 0:     #index is 4,3,2,1,0
+    reverses_string += string[index]    #4,3,2,1,0 
+    index -= 1      #first print i then m then u then h then b
+print(reverses_string)      #display reverse string
