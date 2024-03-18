a0ef7ae9cc317bdc8464896f4fa7849c1b0c79a6
unknown <pambharbhumi108@gmail.com>
2024-03-18 18:55:57 +0530

    18-03-2024

---
+++ Module-3/check_one_comman_member_inTwoList_return_true.py
@@ -0,0 +1,23 @@
+# Write a Python function that takes two lists and returns true if they have at least one common member.
+
+list1 = []
+n1 = int(input("Enter number of element : "))
+
+for i in range(n1):
+    element1 = input("Enter element : ")
+    list1.append(element1)
+
+list2 = []
+n2 = int(input("Enter number of element : "))
+
+for i in range(n2):
+    element2 = input("Enter element : ")
+    list2.append(element2)
+
+for element in list1:
+    if element in list2:
+        print("True")
+        print(f"one common member is : {element}")
+        break
+else:
+    print("False")
