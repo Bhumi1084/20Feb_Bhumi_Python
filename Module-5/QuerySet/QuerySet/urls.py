7f6587c2835a1572e5873b470b10ceee4213ac53
PambharBhumi_HTML_CSS <patelbhumi2504@gmail.com>
2024-06-24 23:05:05 +0530

    24-06-2024

--- Module-5/QuerySet/QuerySet/urls.py
+++ Module-5/QuerySet/QuerySet/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
+    path('showdata/',views.showdata,name='showdata'),
+    path('updatedata/<int:id>',views.updatedata),
+    path('deletedata/<int:id>',views.deletedata),
 ]
--- Module-5/QuerySet/myapp/views.py
+++ Module-5/QuerySet/myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import *
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        req = studform(request.POST)
+        if req.is_valid():
+            req.save()
+            print("Record Inserted..!!")
+        else:
+            print(req.errors)
+    return render(request,'index.html')
+
+def showdata(request):
+    stdata = studinfo.objects.all()
+    return render(request,'showdata.html',{'stdata':stdata})
+
+def updatedata(request,id):
+    stid = studinfo.objects.get(id=id)
+    if request.method == 'POST':
+        stdata=studform(request.POST,instance=stid)
+        if stdata.is_valid():
+            stdata.save()
+            print("Record updated!")
+            return redirect('showdata')
+        else:
+            print(stdata.errors)
+    return render(request,'updatedata.html',{'stid':stid})
+
+def deletedata(request,id):
+    stid = studinfo.objects.get(id=id)
+    studinfo.delete(stid)
+    return redirect('showdata')
