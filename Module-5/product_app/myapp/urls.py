00c1ec6d80ae1ddc6a2e52912bc0f475edb49460
PambharBhumi_HTML_CSS <patelbhumi2504@gmail.com>
2024-07-16 21:59:10 +0530

    16-07-2024

--- Module-5/product_app/myapp/urls.py
+++ Module-5/product_app/myapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,include
 from myapp import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index),
-    path('product_manager/',views.product_manager),   
+    path('product_manager_add/',views.product_manager_add),   
+    path('padmin_add/',views.padmin_add),
     path('padmin/',views.padmin),
+    path('padmin_show/',views.padmin_show,name='showdata'),
+    path('padmin_update/<int:id>',views.padmin_update),
+    path('padmin_delete/<int:id>',views.padmin_delete),
 ]
--- Module-5/product_app/myapp/views.py
+++ Module-5/product_app/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, redirect
 #from product_app import settings
 from .forms import *
 
@@ -7,7 +7,7 @@ from .forms import *
 def index(request):
     return render(request,'index.html')
 
-def padmin(request):
+def padmin_add(request):
     if request.method == 'POST':
         p_admin = pro_admin(request.POST)
         if p_admin.is_valid():
@@ -16,9 +16,9 @@ def padmin(request):
             #msg1 = "Product Add Successfully."
         else:
             print(p_admin.errors)
-    return render(request,'padmin.html')
+    return render(request,'padmin_add.html')
 
-def product_manager(request):
+def product_manager_add(request):
     pnmData = product_master.objects.all()
     
     if request.method == 'POST':
@@ -29,4 +29,28 @@ def product_manager(request):
             #msg = "Sub Product Add Successfully."
         else:
             print(pro_manage.errors)  
-    return render(request,'product_manager.html',{'pnmData' : pnmData})
+    return render(request,'product_manager_add.html',{'pnmData' : pnmData})
+
+def padmin(request):
+    return render(request,'padmin.html')
+
+def padmin_update(request,id):
+    pid = product_master.objects.get(id=id)
+    if request.method=='POST':
+        padmin_data = pro_admin(request.POST,instance=pid)
+        if padmin_data.is_valid():
+            padmin_data.save()
+            print("Recored Updated...!")
+            return redirect('showdata')
+        else:
+            print(padmin_data.errors)
+    return render(request,'padmin_update.html',{'pid':pid})
+
+def padmin_show(request):
+    padmin_data = product_master.objects.all()
+    return render(request,'padmin_show.html',{'padmin_data':padmin_data})
+
+def padmin_delete(request,id):
+    pid = product_master.objects.get(id=id)
+    product_master.delete(pid)
+    return redirect('showdata')
